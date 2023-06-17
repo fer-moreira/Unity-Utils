26bd8b35860b43405a6bc4c69f2b5fbdcbcf16e9
Fernando Moreira <oakley-hbs@hotmail.com>
2023-06-17 15:45:40 -0300

    Update State.cs

--- Scripts/StateMachine/State.cs
+++ Scripts/StateMachine/State.cs
@@ -1,20 +1,11 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class StateRunner<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class State<T> : ScriptableObject where T : MonoBehaviour
 {
-    private State<T> _activeState;
-
-    public void SetState(State<T> newState)
-    {
-        if (_activeState != null)
-            _activeState.Exit();
-
-        _activeState = newState;
-        _activeState.Initialize(GetComponent<T>());
-    }
-
-    private void Update () => _activeState.Update();
-    private void FixedUpdate () => _activeState.FixedUpdate();
+    protected T _runner;
+    
+    public virtual void Initialize (T parent) => _runner = parent;
+    public abstract void Update();
+    public abstract void FixedUpdate();
+    public abstract void Exit();
 }
