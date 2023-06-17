18eaf952779321eaac468e880b98c138b9e531b6
Fernando Moreira <oakley-hbs@hotmail.com>
2023-06-17 15:47:18 -0300

    Update PlayerRunner.cs

--- Scripts/StateMachine/PlayerRunner.cs
+++ Scripts/StateMachine/PlayerRunner.cs
@@ -1,34 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Gigachad.StateMachine;
 
-[RequireComponent(typeof(Rigidbody)), 
-RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class PlayerRunner : StateRunner<PlayerRunner>
 {
-    [SerializeField] private float m_GroundDistance=1;
+    public State<PlayerRunner> m_WalkState;
+    public State<PlayerRunner> m_RunState;
 
-    public Rigidbody2D CachedRigidbody {get; private set;}
-    public Collider2D CachedCollider {get; private set;}
-    public Transform CacahedTransform {get; private set;}
+    public Rigidbody        CachedRigidbody     { get; private set; }
+    public Transform        CacahedTransform    { get; private set; }
+    public CapsuleCollider  CachedCollider      { get; private set; }
 
-    protected override void Start() {
-        CachedRigidbody = GetComponent<Rigidbody2D>();
-        CachedCollider = GetComponent<Collider2D>();
+    protected void Start()
+    {
+        CachedRigidbody = GetComponent<Rigidbody>();
+        CachedCollider = GetComponent<CapsuleCollider>();
         CacahedTransform = this.transform;
-        
-        base.Start();
-    }
 
-    public bool Grounded => Physics2D.Raycast(CacahedTransform.position, Vector2.down * m_GroundDistance);
-
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-
-        if (!CacahedTransform)
-            Gizmos.DrawRay(transform.position, Vector3.down * m_GroundDistance);
-        else
-            Gizmos.DrawRay(CacahedTransform.position, Vector3.down * m_GroundDistance);
+        base.SetState(m_WalkState);
     }
 }
