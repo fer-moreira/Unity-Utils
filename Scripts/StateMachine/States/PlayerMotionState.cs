25dd91337b96a91b7204bd8b5ad2f3e9b9a6773b
Fernando Moreira <oakley-hbs@hotmail.com>
2023-06-17 15:50:23 -0300

    Update PlayerMotionState.cs

--- Scripts/StateMachine/States/PlayerMotionState.cs
+++ Scripts/StateMachine/States/PlayerMotionState.cs
@@ -1,49 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Gigachad.StateMachine;
-using Gigachad;
 
 [CreateAssetMenu(fileName = "STATE_PLAYER_MOTION", menuName = "States/Player/Motion")]
 public class PlayerMotionState : State<PlayerRunner>
 {
-    [SerializeField] private float m_MovmentSpeed=4;
-
-    private float _horizontal;
-    private Rigidbody2D _rigidbody;
-    private Vector2 _velocity;
-
-    public override void Init(PlayerRunner parent)
+    public override void Initialize(RobotStateRunner parent)
     {
-        base.Init(parent);
-        _rigidbody = _runner.CachedRigidbody;
-    }
-
-    public override void UpdateInput()
-    {
-        _horizontal = InputReader.GetAxis("Horizontal");
+        base.Initialize(parent);
     }
 
     public override void Exit()
     {
-
     }
 
     public override void FixedUpdate()
     {
-        _velocity = Vector2.right * _horizontal;
-        _velocity *= (m_MovmentSpeed * 100) * Time.fixedDeltaTime;
-
-        _velocity.y = this._rigidbody.velocity.y;
-
-        this._rigidbody.AddForce(_velocity, ForceMode2D.Force);
     }
 
     public override void Update()
     {
-        if (InputReader.GetButtonDown("Jump"))
-        {
-            Debug.Log("TESTE");
-        }
     }
 }
